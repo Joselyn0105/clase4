2ebcff5452141d7ba37d8f310f73df9d91f19b79
Joselyn0105 <jsendara@istlatroncal.edu.ec>
2025-10-14 18:52:32 -0500

    Deber corrección}

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,5 +1,1 @@
-print("Hola!!!!")
-ciclo = ["Poo", "Base de datos 2", "Metodología de desarrollo de software", "Diseño Multimedia", "Matematicas Aplicada 2"]
-notas = ["9", "9.5", "9", "9", "9.5"]
-
-docentes = ["Israel Surita", "Cecilia Naula"]
+print("Hola 14 de Octubre!!!")
